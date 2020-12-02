d608ca4fb143dd3a2500277dd37c275368789e4b
Samuel Lewis <slewis@atlassian.com>
2020-12-02 16:26:10 +1100

    Day02

---
+++ day02/part1.js
@@ -0,0 +1,17 @@
+const { test } = require('../utils');
+
+const reg = /(\d+)-(\d+) (\w): (.*)/g;
+const solution = (input) => {
+    return input.split('\n').filter(line => {
+        if (!line) {
+            return false;
+        }
+
+        const [_, min, max, char, password] = [...line.matchAll(reg)][0];
+        const freq = (password.match(new RegExp(char,'g')) || []).length;
+
+        return (min <= freq && freq <= max);
+    }).length;
+}
+
+test('Day02 - Part 1', solution, 2);
---
+++ day02/part2.js
@@ -0,0 +1,18 @@
+const { test } = require('../utils');
+
+const reg = /(\d+)-(\d+) (\w): (.*)/g;
+const solution = (input) => {
+    return input.split('\n').filter(line => {
+        if (!line) {
+            return false;
+        }
+
+        let [_, pos1, pos2, char, password] = [...line.matchAll(reg)][0];
+        pos1--;
+        pos2--;
+
+        return (password[pos1] === char && password[pos2] !== char) || (password[pos1] !== char && password[pos2] === char);
+    }).length;
+}
+
+test('Day02 - Part 2', solution, 1);
